c36bcf8fc3d3f0bbe0c812f0faae92727993e17a
mvictoras <mvictoras@gmail.com>
2013-11-15 16:05:47 -0600

    Dependency fix

--- external/UseBullet.cmake
+++ external/UseBullet.cmake
@@ -5,26 +5,26 @@
 #		Add to target_link_libraries
 
 set(BULLET_GENERATOR ${CMAKE_GENERATOR})
-if(WIN32)
-	set(BULLET_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-else()
-	set(BULLET_CXX_FLAGS ${CMAKE_CXX_FLAGS} -fPIC)
-endif()
+
+set(BULLET_CXX_FLAGS ${CMAKE_CXX_FLAGS} -fPIC)
 
 ExternalProject_Add(
 	bullet
 	URL ${CMAKE_SOURCE_DIR}/modules/omegaOsg/external/bullet-2.81-rev2613.tar.gz
 	CMAKE_GENERATOR ${BULLET_GENERATOR}
 	CMAKE_ARGS 
-		-DCMAKE_CXX_FLAGS:STRING=${BULLET_CXX_FLAGS}
-		-DCMAKE_BUILD_TYPE:STRING=Release
+		-DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS}
+    -DCMAKE_CXX_FLAGS:STRING=${BULLET_CXX_FLAGS}
+		-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
 		-DBUILD_AMD_OPENCL_DEMOS=OFF
 		-DBUILD_CPU_DEMOS=OFF
 		-DBUILD_DEMOS=OFF
-    #-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_SOURCE_DIR}/modules/omegaOsg/osgBullet-prefix/src/bullet-install
-    #INSTALL_COMMAND ${PLATFORM_INSTALL_COMMAND}
-    INSTALL_COMMAND ""
-	)
+    -DPKGCONFIG_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-install/lib/pckconfig 
+    -DINCLUDE_INSTALL_DIR=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-install/include
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-install
+  INSTALL_COMMAND ${PLATFORM_INSTALL_COMMAND}
+)
+
 set_target_properties(bullet PROPERTIES FOLDER "3rdparty")
 
 set(BULLET_BASE_DIR ${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src)
@@ -41,17 +41,10 @@ if(OMEGA_OS_WIN)
 		set(${C}_LIBRARY_DEBUG ${BULLET_LIB_DIR}/Debug/${C}_Debug.lib)
 		set(BULLET_LIBS ${BULLET_LIBS} optimized ${${C}_LIBRARY} debug ${${C}_LIBRARY_DEBUG})
 	endforeach()
-elseif(OMEGA_OS_LINUX)
-	# on Linux, libs end up in a directory with theyr same name, instead of the common 'lib' dir.
-    foreach( C ${BULLET_COMPONENTS} )
-		set(${C}_LIBRARY ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a CACHE INTERNAL "")
-		set(${C}_LIBRARY_DEBUG ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a CACHE INTERNAL "")
-		set(BULLET_LIBS ${BULLET_LIBS} optimized ${${C}_LIBRARY} debug ${${C}_LIBRARY_DEBUG} CACHE INTERNAL "")
-	endforeach()
-elseif(APPLE)
+else()
 	foreach( C ${BULLET_COMPONENTS} )
-		set(${C}_LIBRARY ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a CACHE INTERNAL "")
-		set(${C}_LIBRARY_DEBUG ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a CACHE INTERNAL "")
-		set(BULLET_LIBS ${BULLET_LIBS} optimized ${${C}_LIBRARY} debug ${${C}_LIBRARY_DEBUG} CACHE INTERNAL "")
+		set(${C}_LIBRARY ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a)
+		set(${C}_LIBRARY_DEBUG ${BULLET_BASE_DIR}/bullet-build/src/${C}/lib${C}.a)
+		set(BULLET_LIBS ${BULLET_LIBS} optimized ${${C}_LIBRARY} debug ${${C}_LIBRARY_DEBUG})
 	endforeach()
 endif(OMEGA_OS_WIN)
--- external/UseOsgBullet.cmake
+++ external/UseOsgBullet.cmake
@@ -4,236 +4,71 @@ include(${CMAKE_CURRENT_LIST_DIR}/UseBullet.cmake)
 # Pro Trick here: we can't pass the string directly as a CMAKE_ARG in 
 # ExternalProject_Add, because it would keep the double quotes, and we
 # do not want them. Passing it as a variable removes the dobule quotes.
-set(BulletInstallType "Source And Build Tree")
+set(BulletInstallType "Alternate Install Location")
 set(OsgInstallType "Source And Build Tree")
+
+if(WIN32 OR APPLE)
+  set(OSGWORKS_LIB_SUFFIX lib)
+else()
+  set(OSGWORKS_LIB_SUFFIX lib64)
+endif()
+
+set(OSGBULLET_ARGS
+  -DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS}
+  -DCMAKE_CXX_FLAGS:STRING=${OSGBullet_CXX_FLAGS}
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}/osg/osgPlugins-3.3.0
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/osg/osgPlugins-3.3.0
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+  -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+  -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/osg/osgPlugins-3.3.0
+
+  -DOSGInstallType:STRING=${OsgInstallType}
+  -DOSGSourceRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg
+  -DOSGBuildRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg-build
+  
+  -DBUILD_SHARED_LIBS=ON
+  -DOSGBULLET_BUILD_APPLICATIONS=OFF
+  -DOSGBULLET_BUILD_EXAMPLES=OFF
+  -DOSGBULLET_INSTALL_DATA=OFF
+  #Bullet
+  -DBulletInstallType:STRING=${BulletInstallType}
+  -DBulletSourceRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet
+  -DBulletBuildRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-build
+  -DBulletInstallLocation=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-install
+  #----------
+  # NOTE: on linux, the osgWorks config files go in the lib or lib64 directory depending on the bitness.
+  # This means we will need to take this into accout when building for 32 bit linux. 
+  #osgWorks
+  -DosgWorks_DIR:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/osgWorks-prefix/src/osgWorks-build/${OSGWORKS_LIB_SUFFIX}
+  #----------
+  -DOSG_DIR:PATH=${OSG_INSTALL_DIR}
+)
 
 # The OSGWORKS_STATIC preprocessor definition tells osgBullet that
 # we are using the static version of osgWorks.
 if(WIN32)
 	set(OSGBullet_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D\"OSGWORKS_STATIC\"")
+  set(OSGBULLET_ARGS
+    -DCMAKE_SHARED_LINKER_FLAGS:STRING="${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:msvcprt.lib /NODEFAULTLIB:libcpmt.lib"
+		-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
+    ${OSGBULLET_ARGS}
+  )
 else()
 	set(OSGBullet_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DOSGWORKS_STATIC")
 endif()
 
-if(WIN32)
-	ExternalProject_Add(
-		osgBullet
-		URL ${CMAKE_SOURCE_DIR}/modules/omegaOsg/external/osgbullet.tar.gz
-		CMAKE_GENERATOR ${OSGWORKS_GENERATOR}
-		DEPENDS bullet osgWorks osg
-		CMAKE_ARGS 
-			-DCMAKE_SHARED_LINKER_FLAGS:STRING="${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:msvcprt.lib /NODEFAULTLIB:libcpmt.lib"
-			-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
-			-DCMAKE_CXX_FLAGS:STRING=${OSGBullet_CXX_FLAGS}
-			-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}
-
-			-DOSGInstallType:STRING=${OsgInstallType}
-			-DOSGSourceRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg
-			-DOSGBuildRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg-build
-			
-			-DBUILD_SHARED_LIBS:BOOLEAN=false
-			-DOSGBULLET_BUILD_APPLICATIONS=OFF
-			-DOSGBULLET_BUILD_EXAMPLES=OFF
-			-DOSGBULLET_INSTALL_DATA=OFF
-			#Bullet
-			-DBulletInstallType:STRING=${BulletInstallType}
-			-DBulletSourceRoot:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet
-			-DBulletBuildRoot:PATH=${CMAKE_BINARY_DIR}/src/bullet-prefix/src/bullet-build
-			#osgWorks
-			-DosgWorks_DIR:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/osgWorks-prefix/src/osgWorks-build/lib
-			#osg
-			-DOSG_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSG_LIBRARY:PATH=${osg_LIBRARY}
-			-DOSG_LIBRARY_DEBUG:PATH=${osg_LIBRARY_DEBUG}
-			#osgGA
-			-DOSGGA_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGGA_LIBRARY:PATH=${osgGA_LIBRARY}
-			-DOSGGA_LIBRARY_DEBUG:PATH=${osgGA_LIBRARY_DEBUG}
-			#osgText
-			-DOSGTEXT_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGTEXT_LIBRARY:PATH=${osgText_LIBRARY}
-			-DOSGTEXT_LIBRARY_DEBUG:PATH=${osgText_LIBRARY_DEBUG}
-			#osgViewer
-			-DOSGVIEWER_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGVIEWER_LIBRARY:PATH=${osgViewer_LIBRARY}
-			-DOSGVIEWER_LIBRARY_DEBUG:PATH=${osgViewer_LIBRARY_DEBUG}
-			#osgSim
-			-DOSGSIM_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGSIM_LIBRARY:PATH=${osgSim_LIBRARY}
-			-DOSGSIM_LIBRARY_DEBUG:PATH=${osgSim_LIBRARY_DEBUG}
-			#osgDB
-			-DOSGDB_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGDB_LIBRARY:PATH=${osgDB_LIBRARY}
-			-DOSGDB_LIBRARY_DEBUG:PATH=${osgDB_LIBRARY_DEBUG}
-			#osgUtil
-			-DOSGUTIL_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGUTIL_LIBRARY:PATH=${osgUtil_LIBRARY}
-			-DOSGUTIL_LIBRARY_DEBUG:PATH=${osgUtil_LIBRARY_DEBUG}
-			#osgShadow
-			-DOSGSHADOW_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGSHADOW_LIBRARY:PATH=${osgShadow_LIBRARY}
-			-DOSSHADOW_LIBRARY_DEBUG:PATH=${osgShadow_LIBRARY_DEBUG}
-			#openthreads
-			-DOPENTHREADS_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOPENTHREADS_LIBRARY:PATH=${OpenThreads_LIBRARY}
-			-DOPENTHREADS_LIBRARY_DEBUG:PATH=${OpenThreads_LIBRARY_DEBUG}
-			INSTALL_COMMAND ""
-		)
-elseif(APPLE)
-	ExternalProject_Add(
-		osgBullet
-		URL ${CMAKE_SOURCE_DIR}/modules/omegaOsg/external/osgbullet.tar.gz
-		CMAKE_GENERATOR ${OSGWORKS_GENERATOR}
-		DEPENDS bullet osgWorks osg
-		CMAKE_ARGS 
-			#-DCMAKE_SHARED_LINKER_FLAGS:STRING="${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:msvcprt.lib /NODEFAULTLIB:libcpmt.lib"
-			#-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
-			-DCMAKE_CXX_FLAGS:STRING=${OSGBullet_CXX_FLAGS}
-			-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}
-
-			-DOSGInstallType:STRING=${OsgInstallType}
-			-DOSGSourceRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg
-			-DOSGBuildRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg-build
-			
-			# Bild a shared lib on linux, or linking with apps will fail (why? Because little
-			# gnomes live in your computer to make your life miserable)
-			-DBUILD_SHARED_LIBS:BOOLEAN=false
-			-DOSGBULLET_BUILD_APPLICATIONS=OFF
-			-DOSGBULLET_BUILD_EXAMPLES=OFF
-			-DOSGBULLET_INSTALL_DATA=OFF
-			#Bullet
-			-DBulletInstallType:STRING=${BulletInstallType}
-			-DBulletSourceRoot:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet
-			-DBulletBuildRoot:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-build
-			#osgWorks
-			-DosgWorks_DIR:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/osgWorks-prefix/src/osgWorks-build/lib
-			#osg
-			-DOSG_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSG_LIBRARY:PATH=${osg_LIBRARY}
-			-DOSG_LIBRARY_DEBUG:PATH=${osg_LIBRARY_DEBUG}
-			#osgGA
-			-DOSGGA_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGGA_LIBRARY:PATH=${osgGA_LIBRARY}
-			-DOSGGA_LIBRARY_DEBUG:PATH=${osgGA_LIBRARY_DEBUG}
-			#osgText
-			-DOSGTEXT_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGTEXT_LIBRARY:PATH=${osgText_LIBRARY}
-			-DOSGTEXT_LIBRARY_DEBUG:PATH=${osgText_LIBRARY_DEBUG}
-			#osgViewer
-			-DOSGVIEWER_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGVIEWER_LIBRARY:PATH=${osgViewer_LIBRARY}
-			-DOSGVIEWER_LIBRARY_DEBUG:PATH=${osgViewer_LIBRARY_DEBUG}
-			#osgSim
-			-DOSGSIM_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGSIM_LIBRARY:PATH=${osgSim_LIBRARY}
-			-DOSGSIM_LIBRARY_DEBUG:PATH=${osgSim_LIBRARY_DEBUG}
-			#osgDB
-			-DOSGDB_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGDB_LIBRARY:PATH=${osgDB_LIBRARY}
-			-DOSGDB_LIBRARY_DEBUG:PATH=${osgDB_LIBRARY_DEBUG}
-			#osgUtil
-			-DOSGUTIL_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGUTIL_LIBRARY:PATH=${osgUtil_LIBRARY}
-			-DOSGUTIL_LIBRARY_DEBUG:PATH=${osgUtil_LIBRARY_DEBUG}
-			#osgShadow
-			-DOSGSHADOW_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOSGSHADOW_LIBRARY:PATH=${osgShadow_LIBRARY}
-			-DOSGSHADOW_LIBRARY_DEBUG:PATH=${osgShadow_LIBRARY_DEBUG}
-			#openthreads
-			-DOPENTHREADS_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-			-DOPENTHREADS_LIBRARY:PATH=${OpenThreads_LIBRARY}
-			-DOPENTHREADS_LIBRARY_DEBUG:PATH=${OpenThreads_LIBRARY_DEBUG}
-			# bullet
-			-DBULLET_DYNAMICS_LIBRARY:PATH=${BulletDynamics_LIBRARY}
-			-DBULLET_COLLISION_LIBRARY:PATH=${BulletCollision_LIBRARY}
-			-DBULLET_SOFTBODY_LIBRARY:PATH=${BulletSoftBody_LIBRARY}
-			-DBULLET_MATH_LIBRARY:PATH=${LinearMath_LIBRARY}
-			INSTALL_COMMAND ""
-		)
-else()
-	ExternalProject_Add(
-		osgBullet
-		URL ${CMAKE_SOURCE_DIR}/modules/omegaOsg/external/osgbullet.tar.gz
-		CMAKE_GENERATOR ${OSGWORKS_GENERATOR}
-		DEPENDS bullet osgWorks osg
-		CMAKE_ARGS 
-			#-DCMAKE_SHARED_LINKER_FLAGS:STRING="${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:msvcprt.lib /NODEFAULTLIB:libcpmt.lib"
-			#-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
-			-DCMAKE_CXX_FLAGS:STRING=${OSGBullet_CXX_FLAGS}
-			-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}
-			-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}
-
-			-DOSGInstallType:STRING=${OsgInstallType}
-			-DOSGSourceRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg
-			-DOSGBuildRoot:STRING=${CMAKE_BINARY_DIR}/modules/omegaOsg/osg-prefix/src/osg-build
-			
-			# Bild a shared lib on linux, or linking with apps will fail (why? Because little
-			# gnomes live in your computer to make your life miserable)
-			-DBUILD_SHARED_LIBS:BOOLEAN=true
-			-DOSGBULLET_BUILD_APPLICATIONS=OFF
-			-DOSGBULLET_BUILD_EXAMPLES=OFF
-			-DOSGBULLET_INSTALL_DATA=OFF
-			#Bullet
-			-DBulletInstallType:STRING=${BulletInstallType}
-			-DBulletSourceRoot:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet
-			-DBulletBuildRoot:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/bullet-prefix/src/bullet-build
-#----------
-# NOTE: on linux, the osgWorks config files go in the lib or lib64 directory depending on the bitness.
-# This means we will need to take this into accout when building for 32 bit linux. 
-			#osgWorks
-			-DosgWorks_DIR:PATH=${CMAKE_BINARY_DIR}/modules/omegaOsg/osgWorks-prefix/src/osgWorks-build/lib64
-#----------
-			-DOSG_DIR:PATH=${OSG_INSTALL_DIR}
-      #osg
-      #-DOSG_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSG_LIBRARY:PATH=${osg_LIBRARY}
-      #-DOSG_LIBRARY_DEBUG:PATH=${osg_LIBRARY_DEBUG}
-			#osgGA
-      #-DOSGGA_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGGA_LIBRARY:PATH=${osgGA_LIBRARY}
-      #-DOSGGA_LIBRARY_DEBUG:PATH=${osgGA_LIBRARY_DEBUG}
-			#osgText
-      #-DOSGTEXT_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGTEXT_LIBRARY:PATH=${osgText_LIBRARY}
-      #-DOSGTEXT_LIBRARY_DEBUG:PATH=${osgText_LIBRARY_DEBUG}
-			#osgViewer
-      #-DOSGVIEWER_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGVIEWER_LIBRARY:PATH=${osgViewer_LIBRARY}
-      #-DOSGVIEWER_LIBRARY_DEBUG:PATH=${osgViewer_LIBRARY_DEBUG}
-			#osgSim
-      #-DOSGSIM_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGSIM_LIBRARY:PATH=${osgSim_LIBRARY}
-      #-DOSGSIM_LIBRARY_DEBUG:PATH=${osgSim_LIBRARY_DEBUG}
-			#osgDB
-      #-DOSGDB_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGDB_LIBRARY:PATH=${osgDB_LIBRARY}
-      #-DOSGDB_LIBRARY_DEBUG:PATH=${osgDB_LIBRARY_DEBUG}
-			#osgUtil
-      #-DOSGUTIL_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGUTIL_LIBRARY:PATH=${osgUtil_LIBRARY}
-      #-DOSGUTIL_LIBRARY_DEBUG:PATH=${osgUtil_LIBRARY_DEBUG}
-			#osgShadow
-      #-DOSGSHADOW_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOSGSHADOW_LIBRARY:PATH=${osgShadow_LIBRARY}
-      #-DOSSHADOW_LIBRARY_DEBUG:PATH=${osgShadow_LIBRARY_DEBUG}
-			#openthreads
-      #-DOPENTHREADS_INCLUDE_DIR:PATH=${OSG_INCLUDES}
-      #-DOPENTHREADS_LIBRARY:PATH=${OpenThreads_LIBRARY}
-      #-DOPENTHREADS_LIBRARY_DEBUG:PATH=${OpenThreads_LIBRARY_DEBUG}
-			
-			# bullet (linux does not find them automatically?)
-			-DBULLET_DYNAMICS_LIBRARY:PATH=${BulletDynamics_LIBRARY}
-			-DBULLET_COLLISION_LIBRARY:PATH=${BulletCollision_LIBRARY}
-			-DBULLET_SOFTBODY_LIBRARY:PATH=${BulletSoftBody_LIBRARY}
-			-DBULLET_MATH_LIBRARY:PATH=${LinearMath_LIBRARY}
-			INSTALL_COMMAND ""
-		)
-endif()
+ExternalProject_Add(
+	osgBullet
+	DEPENDS bullet osgWorks
+	URL ${CMAKE_SOURCE_DIR}/modules/omegaOsg/external/osgbullet.tar.gz
+	CMAKE_ARGS
+    ${OSGBULLET_ARGS}
+  INSTALL_COMMAND ""
+)
 
 set_target_properties(osgBullet PROPERTIES FOLDER "3rdparty")
 
@@ -257,8 +92,8 @@ elseif(OMEGA_OS_LINUX)
 	endforeach()
 elseif(APPLE)
 	foreach( C ${OSGBULLET_COMPONENTS} )
-		set(${C}_LIBRARY ${OSGBULLET_LIB_DIR}/lib${C}.a)
-		set(${C}_LIBRARY_DEBUG ${OSGBULLET_LIB_DIR}/lib${C}.a)
+		set(${C}_LIBRARY ${OSGBULLET_LIB_DIR}/lib${C}.dylib)
+		set(${C}_LIBRARY_DEBUG ${OSGBULLET_LIB_DIR}/lib${C}.dylib)
 		set(OSGBULLET_LIBRARIES ${OSGBULLET_LIBRARIES} optimized ${${C}_LIBRARY} debug ${${C}_LIBRARY_DEBUG})
 	endforeach()
 endif(OMEGA_OS_WIN)
