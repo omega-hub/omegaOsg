380648cc6bb1956f07a50d4409870602adeac0ff
Alessandro Febretti <febret@gmail.com>
2013-11-18 00:24:02 -0600

    FIX: Windows build NOTE: On windows, we explicitly list all the required bullet and osg libraries, since using the library finding modules does not work correctly in debug builds. the reason is that even for debug builds, release versions of bullet and osg are expected to be found in the installation dirs, making it impossible to generate a debug build without building a release version first (which is annoying)

--- external/UseOsgWorks.cmake
+++ external/UseOsgWorks.cmake
@@ -16,13 +16,52 @@ set(OSGWORKS_ARGS
   -DOSGWORKS_BUILD_APPS:BOOL=OFF
 )
 
-# The OSGWORKS_STATIC preprocessor definition tells osgBullet that
-# we are using the static version of osgWorks.
+# NOTE: On windows, we explicitly list all the required bullet and osg libraries,
+# since using the library finding modules does not work correctly in debug builds.
+# the reason is that even for debug builds, release versions of bullet and osg are
+# expected to be found in the installation dirs, making it impossible to generate
+# a debug build without building a release version first (which is annoying)
 if(WIN32)
+	# The OSGWORKS_STATIC preprocessor definition tells osgBullet that
+	# we are using the static version of osgWorks.
   set(OSGWORKS_ARGS
     -DCMAKE_SHARED_LINKER_FLAGS:STRING="${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:msvcprt.lib /NODEFAULTLIB:libcpmt.lib"
-		-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
-    ${OSGWORKS_ARGS}
+	-DCMAKE_LINKER_FLAGS:STRING="${CMAKE_LINKER_FLAGS} /NODEFAULTLIB:libcpmt.lib /NODEFAULTLIB:msvcprt.lib"
+	-DOSGInstallType:STRING="Alternate Install Location"
+	-DOSGInstallLocation:PATH=${OSG_BINARY_DIR}
+	#osg
+	-DOSG_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSG_LIBRARY:PATH=${osg_LIBRARY}
+	-DOSG_LIBRARY_DEBUG:PATH=${osg_LIBRARY_DEBUG}
+	#osgGA
+	-DOSGGA_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGGA_LIBRARY:PATH=${osgGA_LIBRARY}
+	-DOSGGA_LIBRARY_DEBUG:PATH=${osgGA_LIBRARY_DEBUG}
+	#osgText
+	-DOSGTEXT_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGTEXT_LIBRARY:PATH=${osgText_LIBRARY}
+	-DOSGTEXT_LIBRARY_DEBUG:PATH=${osgText_LIBRARY_DEBUG}
+	#osgViewer
+	-DOSGVIEWER_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGVIEWER_LIBRARY:PATH=${osgViewer_LIBRARY}
+	-DOSGVIEWER_LIBRARY_DEBUG:PATH=${osgViewer_LIBRARY_DEBUG}
+	#osgSim
+	-DOSGSIM_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGSIM_LIBRARY:PATH=${osgSim_LIBRARY}
+	-DOSGSIM_LIBRARY_DEBUG:PATH=${osgSim_LIBRARY_DEBUG}
+	#osgDB
+	-DOSGDB_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGDB_LIBRARY:PATH=${osgDB_LIBRARY}
+	-DOSGDB_LIBRARY_DEBUG:PATH=${osgDB_LIBRARY_DEBUG}
+	#osgUtil
+	-DOSGUTIL_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOSGUTIL_LIBRARY:PATH=${osgUtil_LIBRARY}
+	-DOSGUTIL_LIBRARY_DEBUG:PATH=${osgUtil_LIBRARY_DEBUG}
+	#openthreads
+	-DOPENTHREADS_INCLUDE_DIR:PATH=${OSG_INCLUDES}
+	-DOPENTHREADS_LIBRARY:PATH=${OpenThreads_LIBRARY}
+	-DOPENTHREADS_LIBRARY_DEBUG:PATH=${OpenThreads_LIBRARY_DEBUG}
+	${OSGWORKS_ARGS}
   )
 endif()
 
