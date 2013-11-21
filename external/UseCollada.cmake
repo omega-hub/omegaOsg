dd6360f05eaf3e8735067b756172497cd9933808
mvictoras <mvictoras@gmail.com>
2013-11-21 14:42:48 -0600

    Added linux libraries

--- external/UseCollada.cmake
+++ external/UseCollada.cmake
@@ -3,24 +3,24 @@ if(WIN32)
 	# This is specific to Visual Studio 2010, Win32.
 	# for other versions of GDAL look at http://www.gisinternals.com/sdk/
 	# and add a new section here.
-	set(EXTLIB_NAME gdal)
-	set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
-	set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/gdal)
+  #set(EXTLIB_NAME gdal)
+  #set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
+  #set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/gdal)
+  #	
+  #if(NOT EXISTS ${EXTLIB_DIR})
+  #	message(STATUS "Downloading GDAL library")
+  #		file(DOWNLOAD "https://omegalib.googlecode.com/files/gdal.tar.gz" ${EXTLIB_TGZ} SHOW_PROGRESS)
+  #	execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+  #endif(NOT EXISTS ${EXTLIB_DIR})
 	
-	if(NOT EXISTS ${EXTLIB_DIR})
-		message(STATUS "Downloading GDAL library")
-		file(DOWNLOAD "https://omegalib.googlecode.com/files/gdal.tar.gz" ${EXTLIB_TGZ} SHOW_PROGRESS)
-		execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-	endif(NOT EXISTS ${EXTLIB_DIR})
-	
-	set(GDAL_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
-	set(GDAL_LIBRARY  ${EXTLIB_DIR}/lib/gdal_i.lib CACHE INTERNAL "")
+  #set(GDAL_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
+  #set(GDAL_LIBRARY  ${EXTLIB_DIR}/lib/gdal_i.lib CACHE INTERNAL "")
 	
 	# create phony target gdal
-	add_custom_target(gdal)
+  #add_custom_target(gdal)
 	# Copy the dlls into the target directories
-	file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
-	file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
+  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
+  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
 else()
 	ExternalProject_Add(
 		collada
--- external/UseMinizip.cmake
+++ external/UseMinizip.cmake
@@ -3,25 +3,25 @@ if(WIN32)
 	# This is specific to Visual Studio 2010, Win32.
 	# for other versions of GDAL look at http://www.gisinternals.com/sdk/
 	# and add a new section here.
-	set(EXTLIB_NAME gdal)
-	set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
-	set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/gdal)
+  #set(EXTLIB_NAME gdal)
+  #set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
+  #set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/gdal)
 	
-	if(NOT EXISTS ${EXTLIB_DIR})
-		message(STATUS "Downloading GDAL library")
-		file(DOWNLOAD "https://omegalib.googlecode.com/files/gdal.tar.gz" ${EXTLIB_TGZ} SHOW_PROGRESS)
-		execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-	endif(NOT EXISTS ${EXTLIB_DIR})
+  #if(NOT EXISTS ${EXTLIB_DIR})
+  #	message(STATUS "Downloading GDAL library")
+  #		file(DOWNLOAD "https://omegalib.googlecode.com/files/gdal.tar.gz" ${EXTLIB_TGZ} SHOW_PROGRESS)
+  #	execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+  #endif(NOT EXISTS ${EXTLIB_DIR})
 	
-	set(GDAL_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
-	set(GDAL_LIBRARY  ${EXTLIB_DIR}/lib/gdal_i.lib CACHE INTERNAL "")
+  #set(GDAL_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
+  #set(GDAL_LIBRARY  ${EXTLIB_DIR}/lib/gdal_i.lib CACHE INTERNAL "")
 	
 	# create phony target gdal
-	add_custom_target(gdal)
+  #add_custom_target(gdal)
 	# Copy the dlls into the target directories
-	file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
-	file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
-else(APPLE)
+  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
+  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
+else()
 
     set(MINIZIP_NAME minizip)
     set(MINIZIP_DIR ${CMAKE_BINARY_DIR}/modules/omegaOsg/minizip CACHE INTERNAL "")
@@ -33,7 +33,14 @@ else(APPLE)
 
     # create phony target minizip
     add_custom_target(minizip)
+    
+    if(APPLE)
+        set(LIB_SUFFIX dylib)
+    else()
+        set(LIB_SUFFIX so)
+    endif()
+ 
     set(MINIZIP_INCLUDE_DIR ${MINIZIP_DIR}/osx/include CACHE INTERNAL "")
-    set(MINIZIP_LIBRARY ${MINIZIP_DIR}/osx/lib/libminizip.dylib CACHE INTERNAL "")
+    set(MINIZIP_LIBRARY ${MINIZIP_DIR}/osx/lib/libminizip.${LIB_SUFFIX} CACHE INTERNAL "")
     set(MINIZIP_LIBRARY_PATH ${MINIZIP_DIR}/osx/lib CACHE INTERNAL "")
 endif()
