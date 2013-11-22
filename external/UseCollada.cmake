20ae0a55ab8864314ee1bc0c4aba4f9ae1c6a986
mvictoras <mvictoras@gmail.com>
2013-11-21 19:25:28 -0600

    Windows Fix

--- external/UseCollada.cmake
+++ external/UseCollada.cmake
@@ -1,26 +1,23 @@
 if(WIN32)
 	# On windows we are lazy. Just download precompiled libs. 
 	# This is specific to Visual Studio 2010, Win32.
-	# for other versions of GDAL look at http://www.gisinternals.com/sdk/
-	# and add a new section here.
-  #set(EXTLIB_NAME gdal)
-  #set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
-  #set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/gdal)
+  set(EXTLIB_NAME collada)
+  set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/collada_dom-2.3.1-win32.tar.gz)
+  set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/collada_dom-2.3.1-win32)
   #	
-  #if(NOT EXISTS ${EXTLIB_DIR})
-  #	message(STATUS "Downloading GDAL library")
-  #		file(DOWNLOAD "https://omegalib.googlecode.com/files/gdal.tar.gz" ${EXTLIB_TGZ} SHOW_PROGRESS)
-  #	execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-  #endif(NOT EXISTS ${EXTLIB_DIR})
+  if(NOT EXISTS ${EXTLIB_DIR})
+  	message(STATUS "Unpackiing collada library")
+  	execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+  endif(NOT EXISTS ${EXTLIB_DIR})
 	
-  #set(GDAL_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
-  #set(GDAL_LIBRARY  ${EXTLIB_DIR}/lib/gdal_i.lib CACHE INTERNAL "")
+  set(COLLADA_INCLUDE_DIR ${EXTLIB_DIR}/include CACHE INTERNAL "")
+  set(COLLADA_LIBRARY  ${EXTLIB_DIR}/lib/collada15dom2-vc80-mt.lib CACHE INTERNAL "")
 	
 	# create phony target gdal
-  #add_custom_target(gdal)
+  add_custom_target(gdal)
 	# Copy the dlls into the target directories
-  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
-  #file(COPY ${EXTLIB_DIR}/bin/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
+  file(COPY ${EXTLIB_DIR}/lib/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG} PATTERN "*.dll")
+  file(COPY ${EXTLIB_DIR}/lib/ DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE} PATTERN "*.dll")
 else()
 	ExternalProject_Add(
 		collada
