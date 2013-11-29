1e5518e0f9041c55d4f508a7d0cae2161221aa23
Alessandro Febretti <febret@gmail.com>
2013-11-29 11:32:09 -0600

    FIX: plugin search path

--- src/OsgModule.cpp
+++ src/OsgModule.cpp
@@ -153,7 +153,7 @@ OsgModule::OsgModule():
     String execName;
     StringUtils::splitFilename(execPath, execName, execPath);
     osgDB::Registry* reg = osgDB::Registry::instance();
-    String libPath = execPath + String("/osg/osg/osgPlugins-3.3.0:") + execPath + String("/osg");
+    String libPath = execPath + String("osg/osgPlugins-3.3.0:") + execPath + String("osg");
     ofmsg("OSG Plugin Path(s): %1%", %libPath);
     reg->setLibraryFilePathList(libPath);
 
