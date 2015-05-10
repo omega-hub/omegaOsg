c26e09dfbd807dba06daa901343aff43990805d8
Alessandro Febretti <febret@gmail.com>
2015-05-10 15:56:51 -0500

    NEW: VERSION 2.0 - updated osg, osgWorks and osgBullet to follow current versions from github.

--- pack.cmake
+++ pack.cmake
@@ -1,24 +1,27 @@
+set(OSG_VERSION 3.2.1)
+set(OSG_FILE_VERSION 122)
+
 if(WIN32)
     file(INSTALL DESTINATION ${PACKAGE_DIR}/bin
         TYPE FILE
         FILES
             ${BIN_DIR}/omegaOsg.dll
-            ${BIN_DIR}/osg100-osg.dll
-            ${BIN_DIR}/osg100-osgAnimation.dll
-            ${BIN_DIR}/osg100-osgDB.dll
-            ${BIN_DIR}/osg100-osgFX.dll
-            ${BIN_DIR}/osg100-osgGA.dll
-            ${BIN_DIR}/osg100-osgManipulator.dll
-            ${BIN_DIR}/osg100-osgParticle.dll
-            ${BIN_DIR}/osg100-osgPresentation.dll
-            ${BIN_DIR}/osg100-osgShadow.dll
-            ${BIN_DIR}/osg100-osgSim.dll
-            ${BIN_DIR}/osg100-osgTerrain.dll
-            ${BIN_DIR}/osg100-osgText.dll
-            ${BIN_DIR}/osg100-osgUtil.dll
-            ${BIN_DIR}/osg100-osgViewer.dll
-            ${BIN_DIR}/osg100-osgVolume.dll
-            ${BIN_DIR}/osg100-osgWidget.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osg.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgAnimation.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgDB.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgFX.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgGA.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgManipulator.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgParticle.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgPresentation.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgShadow.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgSim.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgTerrain.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgText.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgUtil.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgViewer.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgVolume.dll
+            ${BIN_DIR}/osg${OSG_FILE_VERSION}-osgWidget.dll
             ${BIN_DIR}/osgbCollision.dll
             ${BIN_DIR}/osgbDynamics.dll
             ${BIN_DIR}/osgbInteraction.dll
@@ -27,6 +30,6 @@ if(WIN32)
     file(INSTALL DESTINATION ${PACKAGE_DIR}/bin
         TYPE DIRECTORY
         FILES
-            ${BIN_DIR}/osgPlugins-3.2.1
+            ${BIN_DIR}/osgPlugins-${OSG_VERSION}
         )
 endif()
